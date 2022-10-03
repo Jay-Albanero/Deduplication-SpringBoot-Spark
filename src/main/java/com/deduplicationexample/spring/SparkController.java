3fdd0494abdc6a739f5204abf3d3bc72ba4764cb
Jay Thakkar <jay.thakkar@forian.com>
2022-10-03 20:08:32 +0530

    commit final code without testing

--- src/main/java/com/deduplicationexample/spring/SparkController.java
+++ src/main/java/com/deduplicationexample/spring/SparkController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.ServletContext;
 import java.io.IOException;
@@ -36,102 +37,81 @@ public class SparkController {
         this.context = context;
     }
 
-    //@GetMapping("")
     @RequestMapping(value = "/read-csv", method = RequestMethod.POST)
-    public ResponseEntity<String> getRowCount(@RequestParam("csv_url") String csv_url,@RequestParam("remove_duplicate_column") String remove_duplicate_column) {
+    public HashMap<String,String> getRowCount(@RequestParam("csv_url") String csv_url,@RequestParam("remove_duplicate_column") String remove_duplicate_column) {
 
-        byte[] array = new byte[7]; // length is bounded by 7
-        new Random().nextBytes(array);
-        String downloaded_filename = new String(array, Charset.forName("UTF-8")) + ".csv";
+        String downloaded_filename = getAlphaNumericString(10) + ".csv";
+        String complete_file_path = System.getProperty("user.dir") + "/savedcsvs" + "/" + downloaded_filename;
+        fileDownloadUsingFilesCopy(csv_url, complete_file_path);
+        Dataset<Row> dataset = this.sparkSession.read().option("header", "true").csv(complete_file_path);
 
-        //String absolutePath = this.context.getRealPath("savedcsvs") + "/" + downloadedfilename;
-        Path absolutePath = Paths.get(this.context.getRealPath("savedcsvs")) ;
-        String absolutePathFile = absolutePath + "/" + downloaded_filename;
+        Dataset<Row> dataset_duplicates_removed = dataset.dropDuplicates(remove_duplicate_column);
 
-        crunchifyFileDownloadUsingFilesCopy(csv_url, System.getProperty("user.dir") + "/savedcsvs" + "/" + downloaded_filename);
+        Dataset<Row> dataset_discarded = dataset.union(dataset_duplicates_removed).except(dataset.intersect(dataset_duplicates_removed));
+        //Dataset<Row> dataset_discarded = dataset.union(dataset_duplicates_removed).except(dataset.intersect(dataset_duplicates_removed));
 
-        URL url = getClass().getClassLoader().getResource("raw_data.csv");
-        Dataset<Row> dataset = this.sparkSession.read().option("header", "true").csv(url.getPath());
+        final String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
 
-        Dataset<Row> dataset_new = dataset.dropDuplicates("Patient Number");
+        // upload filtered file to folder
+        String dataset_duplicates_removed_filename = getAlphaNumericString(10) + ".csv";
 
-        URL url2 = getClass().getClassLoader().getResource("raw_data2.csv");
+        dataset_duplicates_removed.coalesce(1)
+                .write()
+                .option("header","true")
+                .option("sep",",")
+                .mode("overwrite")
+                .csv(System.getProperty("user.dir") + "/savedcsvs" + "/" +  dataset_duplicates_removed_filename);
 
-        //dataset_new.write().format("csv").save(url2.getPath());
-//        dataset_new.coalesce(1)
-//                .write()
-//                .option("header","true")
-//                .option("sep",",")
-//                .mode("overwrite")
-//                .csv("/home/jay/IdeaProjects/Duplication-SpringBoot-Spark/savedcsvs");
+        // upload filtered file to folder
+        String dataset_discarded_filename = getAlphaNumericString(10) + ".csv";
 
-        //Dataset<Row> dataset_new1 = dataset.unionAll(dataset_new).except(dataset.intersect(dataset_new));
-        Dataset<Row> dataset_new1 = dataset.union(dataset_new).except(dataset.intersect(dataset_new));
-        //Dataset<Row> dataset_new1 = dataset.union(dataset_new).except(dataset.intersect(dataset_new));
+        dataset_discarded.coalesce(1)
+                .write()
+                .option("header","true")
+                .option("sep",",")
+                .mode("overwrite")
+                .csv(System.getProperty("user.dir") + "/savedcsvs" + "/" +  dataset_discarded_filename);
 
-//        Dataset<Row> dataset_new1 = dataset.map(row -> {
-//                row.getString();
-//        });
-
-
-
-        String html = String.format("<h1>%s</h1>", "Running Apache Spark on/with support of Spring boot") +
-                //String.format("<h2>%s</h2>", "Spark version = "+sparkSession.sparkContext().version()) +
-                String.format("<h3>%s</h3>", "Read csv..") +
-                String.format("<h4>Total records %d</h4>", dataset.count()) +
-                String.format("<h5>Schema <br/> %s</h5> <br/> Sample data - <br/>", dataset.schema().treeString()) +
-                dataset.showString(20, 20, true)
-
-                +
-
-                String.format("<br><br><br><br><br><br><br><h4>Total records after duplication removed %d</h4>", dataset_new.count()) +
-                String.format("<h5>Schema <br/> %s</h5> <br/> Sample data Duplication Removed - <br/>", dataset_new.schema().treeString()) +
-                dataset_new.showString(20, 20, true)
-
-                +
-
-                String.format("<br><br><br><br><br><br><br><h4>Total records duplicates %d</h4>", dataset_new1.count()) +
-                String.format("<h5>Schema <br/> %s</h5> <br/> Dupliates - <br/>", dataset_new1.schema().treeString()) +
-                dataset_new1.showString(20, 20, true)
-
-//                +
-//
-//                String.format("<br><br><br><br><br><br><br>\\n\\n\\n\\n<h4>Url %d</h4>", url2.getPath())
-
-                ;
-
-//        HashMap<String, String> map = new HashMap<>();
-//        map.put("Message", html);
-//        map.put("csv1", url.getPath());
-//        map.put("csv2", "bb");
-//        return map;
-
-        return ResponseEntity.ok(html);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("duplicate_removed_file_url", baseUrl + "/savedcsvs/" + downloaded_filename);
+        map.put("discarded_file_url", baseUrl + "/savedcsvs/" + dataset_discarded_filename);
+        return map;
     }
 
     // Method-4: File Download using Files.copy()
-    private static void crunchifyFileDownloadUsingFilesCopy(String crunchifyURL, String crunchifyFileLocalPath) {
-
-        // URI Creates a URI by parsing the given string.
-        // This convenience factory method works as if by invoking the URI(String) constructor;
-        // any URISyntaxException thrown by the constructor is caught and wrapped in a new IllegalArgumentException object, which is then thrown.
+    private static void fileDownloadUsingFilesCopy(String crunchifyURL, String crunchifyFileLocalPath) {
         try (InputStream crunchifyInputStream = URI.create(crunchifyURL).toURL().openStream()) {
-
-            // Files: This class consists exclusively of static methods that operate on files, directories, or other types of files.
-            // copy() Copies all bytes from an input stream to a file. On return, the input stream will be at end of stream.
-
-            // Paths: This class consists exclusively of static methods that return a Path by converting a path string or URI.
-            // Paths.get() Converts a path string, or a sequence of strings that when joined form a path string, to a Path.
             Files.copy(crunchifyInputStream, Paths.get(crunchifyFileLocalPath), StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
-            // printStackTrace: Prints this throwable and its backtrace to the standard error stream.
             e.printStackTrace();
         }
-
-        //printResult("File Downloaded Successfully with Files.copy() Operation \n");
     }
 
-    private static void printResult(String s) {
+    private static String getAlphaNumericString(int n)
+    {
+
+        // chose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
     }
 
     @GetMapping("/")
